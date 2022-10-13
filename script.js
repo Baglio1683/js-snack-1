7cfc8dc15c5fff796674fb324f1eadb8743a6660
MyUsername <alberto.baglivi@gmail.com>
2022-10-13 12:48:42 +0200

    Add modification of Snack-1

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-
 let word1 = prompt("inserire la prima parola: ")
 let word2 = prompt("Inserire la seconda parola: ")
 
 if(word1.length > word2.length){
-    alert("la parola più corta è " + word2 + " e quella più lunga è " + word1)
+    alert(`la parola più corta è "${word2}" e quella più lunga è "${word1}"`)
 }
 
 
 if(word1.length < word2.length){
-    alert("la parola più corta è " + word1 + " e quella più lunga è " + word2)
+    alert(`la parola più corta è "${word1}" e quella più lunga è "${word2}"`)
 }
+
+if(word1.length === word2.length){
+    alert(`la parola "${word1}" è della stessa lunghezza della parola "${word2}"`)
+}
+
+
+
